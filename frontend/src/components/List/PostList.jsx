8eacba158d23e90db67cbbf6aedb203754813c14
Park Shinhyung <23102009@seoultech.ac.kr>
2026-01-04 02:45:06 -0800

    feat: 컴포넌트 스타일링

--- frontend/src/components/List/PostList.jsx
+++ frontend/src/components/List/PostList.jsx
@@ -1,16 +1,21 @@
 import "./List.css";
 
-function PostList({ title, date }) {
-  return (
-    <div className="post-list-item">
-      <span className="index">01</span>
-
-      <div>
-        <h4>{title}</h4>
-        <span>{date}</span>
-      </div>
-    </div>
-  );
-}
-
-export default PostList;
+function PostList({ index, title, date, image }) {
+    return (
+      <article className="post-list-item">
+        <div className="post-list-left">
+          <span className="post-list-index">{index}</span>
+          <h3 className="post-list-title">{title}</h3>
+          <span className="post-list-date">{date}</span>
+        </div>
+  
+        {image && (
+          <div className="post-list-image">
+            <img src={image} alt="공지 이미지" />
+          </div>
+        )}
+      </article>
+    );
+  }
+  
+  export default PostList;
