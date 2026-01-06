1254cafdf2c82a5665d198284bfc35dbfdb73268
Donghyun Lim <ehdgus4173@gmail.com>
2026-01-06 22:44:25 +0900

    add controller annotations

--- backend/src/main/java/com/example/backend/news/controller/NewsController.java
+++ backend/src/main/java/com/example/backend/news/controller/NewsController.java
@@ -1,10 +1,11 @@
 package com.example.backend.news.controller;
 
 import com.example.backend.common.CreateResponse;
+import com.example.backend.global.annotation.ApiErrorExceptionsExample;
+import com.example.backend.news.document.*;
 import com.example.backend.news.dto.*;
-//import com.example.backend.news.service.NewsService;
-
 import com.example.backend.news.service.NewsService;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,21 +22,21 @@ public class NewsController {
     private final NewsService newsService;
 
     @GetMapping
+    @ApiErrorExceptionsExample(NewsGetListExceptionDocs.class)
     public ResponseEntity<List<NewsSummaryResponse>> getNewsList() {
-        // TODO
         return ResponseEntity.ok(newsService.getNewsList());
     }
 
     @GetMapping("/{id}")
+    @ApiErrorExceptionsExample(NewsGetDetailExceptionDocs.class)
     public ResponseEntity<NewsDetailResponse> getNews(@PathVariable Long id) {
-        // TODO
         return ResponseEntity.ok(newsService.getNews(id));
     }
 
     @Valid
     @PostMapping
+    @ApiErrorExceptionsExample(NewsCreateExceptionDocs.class)
     public ResponseEntity<CreateResponse> createNews(@RequestBody CreateNewsRequest request) {
-        // TODO
         Long id = newsService.createNews(request);
         return ResponseEntity.status(201).body(new CreateResponse(id, true));
     }
